a4c6300c16879bf42a918964d92997efc240c785
jszipszky <jszipszk@purdue.edu>
2019-12-18 02:23:46 -0500

    added force vector vis on 'special' boids

--- Boid.js
+++ Boid.js
@@ -5,12 +5,17 @@ class Boid {
         this.velocity = p5.Vector.random2D();
         this.velocity.setMag(random(6,10));
         this.acceleration = createVector();
-        this.maxSteeringForce = .25;
-        this.minSpeed = 4.5;
-        this.maxSpeed = 9;
         this.r = random(175, 235);
         this.g = random(175, 235);
         this.b = random(205, 255);
+        this.drawVectors = false;
+
+        this.visualMultiplier = 150;
+        this.maxSteeringForce = .25;
+        this.minSpeed = 4.5;
+        this.maxSpeed = 9;
+        this.perception = 150;
+        this.separation = 60;
     }
 
     show() {
@@ -33,6 +38,7 @@ class Boid {
         this.r = 255;
         this.g = 105;
         this.b = 105;
+        this.drawVectors = true;
     }
 
     update(boids) {
@@ -54,13 +60,12 @@ class Boid {
     }
 
     align(boids) {
-        let perception = 150;
         let steeringForce = createVector(0, 0);
         let avg = createVector();
         let numPerceived = 0;
         for (let boid of boids) {
             let d = dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (boid != this && d <= perception) {
+            if (boid != this && d <= this.perception) {
                 numPerceived++;
                 avg.add(boid.velocity);
             }
@@ -70,18 +75,21 @@ class Boid {
             avg.setMag(this.maxSpeed);
             steeringForce = avg.sub(this.velocity);
             steeringForce.limit(this.maxSteeringForce);
+            if (this.drawVectors) {
+                var v = p5.Vector.mult(steeringForce, this.visualMultiplier);
+                this.drawArrow(this.position, v, 'purple');
+            }
         }
         return steeringForce;
     }
 
     cohere(boids) {
-        let perception = 150;
         let steeringForce = createVector(0, 0);
         let avg = createVector();
         let numPerceived = 0;
         for (let boid of boids) {
             let d = dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (boid != this && d <= perception) {
+            if (boid != this && d <= this.perception) {
                 numPerceived++;
                 avg.add(boid.position);
             }
@@ -92,18 +100,21 @@ class Boid {
             avg.setMag(this.maxSpeed);
             steeringForce = avg.sub(this.velocity);
             steeringForce.limit(this.maxSteeringForce);
+            if (this.drawVectors) {
+                var v = p5.Vector.mult(steeringForce, this.visualMultiplier);
+                this.drawArrow(this.position, v, 'green');
+            }
         }
         return steeringForce;
     }
 
     separate() {
-        let separation = 60;
         let steeringForce = createVector(0,0);
         let avg = createVector(0,0);
         let numPerceived = 0;
         for (let boid of boids) {
             let d = dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (boid != this &&  d <= separation) {
+            if (boid != this &&  d <= this.separation) {
                 numPerceived++;
                 let v = p5.Vector.sub(this.position, boid.position);
                 v.div(d * d);
@@ -115,9 +126,27 @@ class Boid {
             avg.setMag(this.maxSpeed);
             steeringForce = avg.sub(this.velocity);
             steeringForce.limit(1.5 * this.maxSteeringForce);
+            if (this.drawVectors) {
+                var v = p5.Vector.mult(steeringForce, this.visualMultiplier);
+                this.drawArrow(this.position, v, 'red');
+            }
         }
         return steeringForce;
     }
+
+    drawArrow(base, vec, myColor) {
+        push();
+        stroke(myColor);
+        strokeWeight(3);
+        fill(myColor);
+        translate(base.x, base.y);
+        line(0, 0, vec.x, vec.y);
+        rotate(vec.heading());
+        let arrowSize = 7;
+        translate(vec.mag() - arrowSize, 0);
+        triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
+        pop();
+      }
 
     edges() {
         if (this.position.x > width) {
