d172e509e35604d3683b0ee5bcfb863db1809174
jszipszky <jszipszk@purdue.edu>
2019-12-18 04:01:35 -0500

    add force vector & perception vis to 'special' boids, acceleration code cleanup

--- Boid.js
+++ Boid.js
@@ -16,6 +16,10 @@ class Boid {
         this.maxSpeed = 9;
         this.perception = 150;
         this.separation = 60;
+        this.colisionForceMult = 1.5;
+        this.colisionDistMult = 2;
+        this.perceptionColor = color(44, 228, 215, 255);
+        this.separationColor = color(255, 14, 16, 255);
     }
 
     show() {
@@ -32,6 +36,10 @@ class Boid {
         rotate(this.velocity.heading() + radians(90));
         triangle(x1, y1, x2, y2, x3, y3);
         pop();
+        if (this.drawVectors) {
+            this.drawCircle(this.position, this.separation, this.separationColor);
+            this.drawCircle(this.position, this.perception, this.perceptionColor);
+        }
     }
 
     makeSpecial() {
@@ -43,11 +51,8 @@ class Boid {
 
     update(boids) {
         this.edges();
-        let alignment = this.align(boids);
-        let cohesion = this.cohere(boids);
-        let separation = this.separate(boids);
     
-        this.acceleration = alignment.add(cohesion.add(separation));
+        this.acceleration = this.getAcceleration();
 
         this.velocity.add(this.acceleration);
         let speed = this.velocity.mag();
@@ -59,79 +64,65 @@ class Boid {
         this.position.add(this.velocity);
     }
 
-    align(boids) {
-        let steeringForce = createVector(0, 0);
-        let avg = createVector();
-        let numPerceived = 0;
-        for (let boid of boids) {
-            let d = dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (boid != this && d <= this.perception) {
-                numPerceived++;
-                avg.add(boid.velocity);
-            }
-        }
-        if (numPerceived != 0) {
-            avg.div(numPerceived);
-            avg.setMag(this.maxSpeed);
-            steeringForce = avg.sub(this.velocity);
-            steeringForce.limit(this.maxSteeringForce);
-            if (this.drawVectors) {
-                var v = p5.Vector.mult(steeringForce, this.visualMultiplier);
-                this.drawArrow(this.position, v, 'purple');
-            }
-        }
-        return steeringForce;
-    }
+    getAcceleration() {
+        let alignmentForce = createVector(0, 0);
+        let alignmentAvg = createVector(0, 0);
+        let cohesionForce = createVector(0, 0);
+        let cohesionAvg = createVector(0, 0);
+        let coheseAlignNum = 0;
 
-    cohere(boids) {
-        let steeringForce = createVector(0, 0);
-        let avg = createVector();
-        let numPerceived = 0;
-        for (let boid of boids) {
-            let d = dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (boid != this && d <= this.perception) {
-                numPerceived++;
-                avg.add(boid.position);
-            }
-        }
-        if (numPerceived != 0) {
-            avg.div(numPerceived);
-            avg.sub(this.position);
-            avg.setMag(this.maxSpeed);
-            steeringForce = avg.sub(this.velocity);
-            steeringForce.limit(this.maxSteeringForce);
-            if (this.drawVectors) {
-                var v = p5.Vector.mult(steeringForce, this.visualMultiplier);
-                this.drawArrow(this.position, v, 'green');
-            }
-        }
-        return steeringForce;
-    }
+        let separationForce = createVector(0, 0);
+        let separationAvg = createVector(0, 0);
+        let separationNum = 0;
 
-    separate() {
-        let steeringForce = createVector(0,0);
-        let avg = createVector(0,0);
-        let numPerceived = 0;
         for (let boid of boids) {
             let d = dist(this.position.x, this.position.y, boid.position.x, boid.position.y);
-            if (boid != this &&  d <= this.separation) {
-                numPerceived++;
-                let v = p5.Vector.sub(this.position, boid.position);
-                v.div(d * d);
-                avg.add(v);
+            if (boid != this) {
+                if (d <= this.perception) {
+                    coheseAlignNum++;
+                    alignmentAvg.add(boid.velocity);
+                    cohesionAvg.add(boid.position);
+                }
+                if (d <= this.separation) {
+                    separationNum++;
+
+                    let v = p5.Vector.sub(this.position, boid.position);
+                    v.div(Math.pow(d, this.colisionDistMult));
+                    separationAvg.add(v);
+                }
             }
         }
-        if (numPerceived != 0) {
-            avg.div(numPerceived);
-            avg.setMag(this.maxSpeed);
-            steeringForce = avg.sub(this.velocity);
-            steeringForce.limit(1.5 * this.maxSteeringForce);
-            if (this.drawVectors) {
-                var v = p5.Vector.mult(steeringForce, this.visualMultiplier);
-                this.drawArrow(this.position, v, 'red');
-            }
+
+        if (coheseAlignNum != 0) {
+            alignmentAvg.div(coheseAlignNum);
+            alignmentAvg.setMag(this.maxSpeed);            
+            alignmentForce = alignmentAvg.sub(this.velocity);
+            alignmentForce.limit(this.maxSteeringForce);
+            
+            cohesionAvg.div(coheseAlignNum);
+            cohesionAvg.sub(this.position);
+            cohesionAvg.setMag(this.maxSpeed);
+            cohesionForce = cohesionAvg.sub(this.velocity);
+            cohesionForce.limit(this.maxSteeringForce);
         }
-        return steeringForce;
+
+        if (separationNum != 0) {
+            separationAvg.div(separationNum);
+            separationAvg.setMag(this.maxSpeed);
+            separationForce = separationAvg.sub(this.velocity);
+            separationForce.limit(this.colisionForceMult * this.maxSteeringForce);
+        }
+
+        if (this.drawVectors) {
+            let v = p5.Vector.mult(alignmentForce, this.visualMultiplier);
+            this.drawArrow(this.position, v, 'purple');
+            v = p5.Vector.mult(cohesionForce, this.visualMultiplier);
+            this.drawArrow(this.position, v, 'green');
+            v = p5.Vector.mult(separationForce, this.visualMultiplier);
+            this.drawArrow(this.position, v, 'red');
+        }
+
+        return alignmentForce.add(cohesionForce.add(separationForce));
     }
 
     drawArrow(base, vec, myColor) {
@@ -146,7 +137,18 @@ class Boid {
         translate(vec.mag() - arrowSize, 0);
         triangle(0, arrowSize / 2, 0, -arrowSize / 2, arrowSize, 0);
         pop();
-      }
+    }
+
+    drawCircle(base, radius, myColor) {
+        push();
+        ellipseMode(RADIUS);
+        stroke(myColor);
+        strokeWeight(2);
+        myColor.setAlpha(50);
+        fill(myColor);
+        ellipse(base.x, base.y, radius, radius);
+        pop();
+    }
 
     edges() {
         if (this.position.x > width) {
